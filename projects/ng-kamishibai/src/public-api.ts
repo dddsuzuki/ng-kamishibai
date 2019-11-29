611d93864f8632d5c4a2faed497213fc34918459
Daisuke Suzuki <35087637+dddsuzuki@users.noreply.github.com>
2019-11-29 10:19:27 +0900

    Fix public-api.ts

--- projects/ng-kamishibai/src/public-api.ts
+++ projects/ng-kamishibai/src/public-api.ts
@@ -2,7 +2,8 @@
  * Public API Surface of ng-kamishibai
  */
 
-export { KAMISHIBAI_PAGES } from './lib/page.service';
+export * from './lib/page.service';
+export * from './lib/container.component';
 export * from './lib/kamishibai.service';
 export * from './lib/kamishibai.component';
 export * from './lib/kamishibai.module';
